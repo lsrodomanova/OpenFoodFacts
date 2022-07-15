f59b7c5070a09562d39afc53b552ce06de97a5c8
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-07-15 10:58:24 +0300

    plugin

--- src/test/java/config/BrStackConfig.java
+++ src/test/java/config/BrStackConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-        "classpath:properties/brstack.properties"
+        "classpath:brstack.properties"
 })
 
 public interface BrStackConfig extends Config {
